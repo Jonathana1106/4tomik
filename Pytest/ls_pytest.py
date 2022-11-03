aa8d347f139a9866c62521c85498e026d9056b53
Jonathana1106 <jonathana1196@gmail.com>
2022-11-03 11:04:33 -0600

    [RESTART] repo for runner in C#

--- Pytest/ls_pytest.py
+++
@@ -1,8 +0,0 @@
-
-
-from typing import List
-
-def total(xs: List[float]) -> float:
-    return 0.0
-
-    
--- Pytest/testing/test_ls_pytest.py
+++
@@ -1,6 +0,0 @@
-""" An example of test """
-
-from ls_pytest import total
-
-def test_total_empty() -> None:
-    assert total([]) == 0.0
